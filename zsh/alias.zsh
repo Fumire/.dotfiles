470bd44885e0e9fef6dd1097a2599bd7a92db9d6
Jaewoong Lee <230@fumire.moe>
2019-07-19 08:40:10 +0900

    add ALIAS.ZSH

---
+++ zsh/alias.zsh
@@ -0,0 +1,19 @@
+# common typos
+alias claer='clear'
+alias clera='clear'
+alias celar='clear'
+
+# prevent accident
+alias rm='rm -i'
+alias mv='mv -i'
+alias cp='cp -i'
+
+# Python3
+alias py='python3'
+
+# PIP update
+alias pipUpdate="pip3 freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip3 install -U"
+
+# Shortcuts
+alias copyssh="pbcopy < $HOME/.ssh/id_rsa.pub"
+weather() { curl -4 wttr.in/${1:-seoul} }
