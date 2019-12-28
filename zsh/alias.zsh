181764529ba32484c01266a1b364ef9974ae337b
Jaewoong Lee <230@fumire.moe>
2019-12-28 21:13:43 +0900

    Update alias.zsh
    double spaces

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -12,7 +12,7 @@ alias cp='cp -i'
 alias py='python3'
 
 # PIP update
-alias pipUpdate="pip3 freeze --local | grep -v '^\-e' | cut -d = -f 1  | xargs -n1 pip3 install -U"
+alias pipUpdate="pip3 freeze --local | grep -v '^\-e' | cut -d = -f 1 | xargs -n1 pip3 install -U"
 
 # Shortcuts
 alias copyssh="pbcopy < $HOME/.ssh/id_rsa.pub"
