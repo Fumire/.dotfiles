3e5c568d89f5d30edec5390796e16f29afcbde7e
Jaewoong Lee <230@fumire.moe>
2020-07-14 15:14:12 +0900

    Add make_user.sh

---
+++ utility/make_user.sh
@@ -0,0 +1,50 @@
+#! /bin/ bash
+# Maintainer: jwlee230@unist.ac.kr
+
+read -p "Input User ID: " ID
+if [ -n "${ID}" ]
+then
+    echo "ID is <${ID}>"
+else
+    echo "ID cannot be NULL"
+    exit
+fi
+
+read -p "Input UID(can be NULL): " newUID
+if [ -n "${newUID}" ]
+then
+    echo "UID is <${newUID}>"
+else
+    echo "UID is NULL"
+fi
+
+read -p "Input GID(can be NULL): " newGID
+if [ -n "${newGID}" ]
+then
+    echo "GID is <${newGID}>"
+else
+    echo "GID is NULL"
+fi
+
+if [ -n "`cat /etc/passwd | grep ${ID}`" ]
+then
+    echo "Same ID exists"
+    exit
+fi
+
+if [ -z "${newUID}" -a -z "${newGID}" ]
+then
+    adduser --home /BiO/Live/${ID} --shell /bin/bash ${ID}
+elif [ -n "${newUID}" ]
+then
+    adduser --home /BiO/Live/${ID} --shell /bin/bash -uid ${newUID} ${ID}
+elif [ -n "${newGID}" ]
+then
+    adduser --home /BiO/Live/${ID} --shell /bin/bash -gid ${newGID} ${ID}
+else
+    adduser --home /BiO/Live/${ID} --shell /bin/bash -uid ${newUID} -gid ${newGID} ${ID}
+fi
+
+Original="`cat /etc/group | grep compbio`"
+sed -i "s/${Original}/${Original},${ID}/g" /etc/group
+
