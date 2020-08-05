a03662b935ed62e2f788acb983c6567380675a1b
Jaewoong Lee <230@fumire.moe>
2020-08-05 17:49:06 +0900

    For monitoring server status

---
+++ utility/check_system.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+IDLE_CPU=`top -b -n 1 | grep "\%Cpu(s)" | awk '{ print $8}' | cut -d "." -f 1`
+
+if (( $IDLE_CPU < 20 )); then
+    sar | mail -s "[Warning] CPU Usage is too high" -r "noreply@fumire.moe" "230@fumire.moe"
+else
+    echo "CPU is Okay"
+fi
+
+TOTAL_MEM=`free | awk 'FNR == 2 {print $2}'`
+AVAIL_MEM=`free | awk 'FNR == 2 {print $7}'`
+IDLE_MEM=`echo "$AVAIL_MEM * 100 / $TOTAL_MEM" | bc`
+
+if (( $IDLE_MEM < 20 )); then
+    sar -r | mail -s "[Warning] MEM Usage is too high" -r "noreply@fumire.moe" "230@fumire.moe"
+else
+    echo "MEM is Okay"
+fi
