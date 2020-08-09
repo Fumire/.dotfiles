4ff4e4cfc7eda477b7d60695341ada76ba1fac44
Jaewoong Lee <230@fumire.moe>
2020-08-10 06:37:07 +0900

    Update

--- utility/check_system.sh
+++ utility/check_system.sh
@@ -7,25 +7,29 @@ date
 IDLE_CPU=`top -b -n 1 | grep "\%Cpu(s)" | awk '{ print $8}' | cut -d "." -f 1`
 
 if (( $IDLE_CPU < 10 )); then
-    sar | mail -s "[Error] CPU Usage is too high" -r "noreply@fumire.moe" "230@fumire.moe"
+    sar | mail -s "[Error] CPU Usage is too high" "230@fumire.moe"
+    echo "CPU Error:" $IDLE_CPU
 elif (( $IDLE_CPU < 20 )); then
-    sar | mail -s "[Warning] CPU Usage is too high" -r "noreply@fumire.moe" "230@fumire.moe"
+    sar | mail -s "[Warning] CPU Usage is too high" "230@fumire.moe"
+    echo "CPU Warning:" $IDLE_CPU
 else
     echo "CPU is Okay:" $IDLE_CPU
 fi
 
 TOTAL_MEM=`free | grep "^Mem" | awk '{print $2}'`
 if [[ -z `free | grep "available"` ]]; then
-    ACTUAL_MEM=`free | grep "^-/+" | awk '{print $3}'`
+    ACTUAL_MEM=`free | grep "^-/+" | awk '{print $4}'`
 else
     ACTUAL_MEM=`free | grep "^Mem" | awk '{print $7}'`
 fi
 IDLE_MEM=`echo "$ACTUAL_MEM * 100 / $TOTAL_MEM" | bc`
 
 if (( $IDLE_MEM < 10 )); then
-    free -m | mail -s "[Error] MEM Usage is too high" -r "noreply@fumire.moe" "230@fumire.moe"
+    free -mh | mail -s "[Error] MEM Usage is too high" "230@fumire.moe"
+    echo "MEM Error:" $IDLE_MEM
 elif (( $IDLE_MEM < 20 )); then
-    free -m | mail -s "[Warning] MEM Usage is too high" -r "noreply@fumire.moe" "230@fumire.moe"
+    free -mh | mail -s "[Warning] MEM Usage is too high" "230@fumire.moe"
+    echo "MEM Warning:" $IDLE_MEM
 else
     echo "MEM is Okay:" $IDLE_MEM
 fi
