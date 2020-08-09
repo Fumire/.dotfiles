a45947ebf82ce0786a7cb7fb464696aa7d4ef07e
Jaewoong Lee <230@fumire.moe>
2020-08-10 02:04:02 +0900

    Add Error/Warning

--- utility/check_system.sh
+++ utility/check_system.sh
@@ -6,7 +6,9 @@ date
 
 IDLE_CPU=`top -b -n 1 | grep "\%Cpu(s)" | awk '{ print $8}' | cut -d "." -f 1`
 
-if (( $IDLE_CPU < 20 )); then
+if (( $IDLE_CPU < 10 )); then
+    sar | mail -s "[Error] CPU Usage is too high" -r "noreply@fumire.moe" "230@fumire.moe"
+elif (( $IDLE_CPU < 20 )); then
     sar | mail -s "[Warning] CPU Usage is too high" -r "noreply@fumire.moe" "230@fumire.moe"
 else
     echo "CPU is Okay:" $IDLE_CPU
@@ -20,8 +22,10 @@ else
 fi
 IDLE_MEM=`echo "$ACTUAL_MEM * 100 / $TOTAL_MEM" | bc`
 
-if (( $IDLE_MEM < 20 )); then
-    sar -r | mail -s "[Warning] MEM Usage is too high" -r "noreply@fumire.moe" "230@fumire.moe"
+if (( $IDLE_MEM < 10 )); then
+    free -m | mail -s "[Error] MEM Usage is too high" -r "noreply@fumire.moe" "230@fumire.moe"
+elif (( $IDLE_MEM < 20 )); then
+    free -m | mail -s "[Warning] MEM Usage is too high" -r "noreply@fumire.moe" "230@fumire.moe"
 else
     echo "MEM is Okay:" $IDLE_MEM
 fi
