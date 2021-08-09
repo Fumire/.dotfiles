f35ebb898c01860105e531a32b0fcaa13f1a000f
Jaewoong Lee <230@fumire.moe>
2021-08-10 08:25:14 +0900

    Update parameters

--- utility/reporting.sh
+++ utility/reporting.sh
@@ -5,12 +5,14 @@ for h in 'host1' 'host2'; do
     scp -c aes256-cbc -P $PORT jwlee230@$h.kogic.kr:/var/log/sysstat/sa$(date +%d) $h
 done
 for h in 'host1' 'host2'; do
-    if [[ "$h" != "compbio" ]]; then
+    if [[ "$h" != 'compbio' ]]; then
         sadf -c $h 1> $h.new 2> /dev/null
         rm $h
     else
         mv $h $h.new
     fi
-    sadf -g -O autoscale,showinfo,packed,height=400 -T -- $h.new > $h.svg && rm $h.new
-    convert -density 500 $h.svg /var/www/html/image/top/$h.jpg && rm $h.svg
+    sadf -g -O autoscale,showinfo,packed,height=700 -T -- -r -u $h.new > $h.svg
+    convert -density 500 -crop 5500x3500+0+0 $h.svg /var/www/html/image/top/$h.jpg &
 done
+wait
+rm *.new *.svg
