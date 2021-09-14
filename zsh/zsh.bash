72ae45638d0acea6987b367641663c6955207782
Jaewoong Lee <230@fumire.moe>
2021-09-14 17:43:32 +0900

    Add ZSH related files

--- zsh/zsh.bash
+++ zsh/zsh.bash
@@ -1,7 +1,7 @@
-#!/bin/sh
+#!/usr/bin/env bash
 if [ -e /bin/zsh ]; then
     if [ "$(echo $SHELL)" != "$(which zsh)" ]; then
-        chsh -s `which zsh`
+        chsh -s $(which zsh)
         echo "change SHELL to zsh"
     fi
 elif [ -e /usr/local/bin/zsh ]; then
@@ -11,7 +11,7 @@ else
     if [ ! -e $HOME/bin/zsh ]; then
         curl --output $HOME/zsh.tar.xz -L https://sourceforge.net/projects/zsh/files/latest/download
         cd $HOME && mkdir zsh && unxz zsh.tar.xz && tar -xvf zsh.tar -C zsh --strip-components 1
-        cd $HOME/zsh && ./configure --prefix $HOME && make && make install
+        cd $HOME/zsh && ./configure --prefix $HOME && make -j && make -j install
     fi
     echo "export SHELL=$HOME/bin/zsh" >> $HOME/.bash_profile
     echo "[ -z \"\$ZSH_VERSION\" ] && exec \"\$SHELL\" -l" >> $HOME/.bash_profile
