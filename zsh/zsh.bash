fd62e5056b4a36b53482e77884dd28eb636e75b0
Jaewoong Lee <230@fumire.moe>
2021-09-30 23:01:46 +0900

    If ZSH is not installed, then ...

--- zsh/zsh.bash
+++ zsh/zsh.bash
@@ -1,19 +1,16 @@
 #!/usr/bin/env bash
-if [ -e /bin/zsh ]; then
-    if [ "$(echo $SHELL)" != "$(which zsh)" ]; then
-        chsh -s $(which zsh)
-        echo "change SHELL to zsh"
-    fi
-elif [ -e /usr/local/bin/zsh ]; then
-    echo "export SHELL=/usr/local/bin/zsh\nexec /usr/local/bin/zsh -l" >> $HOME/.bash_profile
-    source $HOME/.bash_profile
+set -euo pipefail
+IFS=$'\n\t'
+if [ $(which zsh > /dev/null; echo $?) = 0 ]; then
+    which $SHELL | grep "zsh" > /dev/null || chsh -s $(which zsh)
 else
-    if [ ! -e $HOME/bin/zsh ]; then
-        curl --output $HOME/zsh.tar.xz -L https://sourceforge.net/projects/zsh/files/latest/download
+    if [ $(uname) = "Darwin" ]; then
+        brew install zsh
+    elif [ $(uname) = "Linux" -a $(whoami) = "root"]; then
+        apt-get update && apt-get install zsh -y
+    else
+        curl --output $HOME/zsh.tar.xz --location https://sourceforge.net/projects/zsh/files/latest/download
         cd $HOME && mkdir zsh && unxz zsh.tar.xz && tar -xvf zsh.tar -C zsh --strip-components 1
         cd $HOME/zsh && ./configure --prefix $HOME && make -j && make -j install
     fi
-    echo "export SHELL=$HOME/bin/zsh" >> $HOME/.bash_profile
-    echo "[ -z \"\$ZSH_VERSION\" ] && exec \"\$SHELL\" -l" >> $HOME/.bash_profile
-    source $HOME/.bash_profile
 fi
