ee5b7047606b374ae265ee4b4203e74e0fa439f2
Jaewoong Lee <230@fumire.moe>
2022-03-24 01:07:23 +0900

    Update ZSH config

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -15,6 +15,8 @@ alias py='python3'
 alias pipUpdate="pip3 freeze --local | grep -v '^\-e' | cut -d = -f 1 | xargs -n1 pip3 install -U"
 
 # Shortcuts
-alias copyssh="pbcopy < $HOME/.ssh/id_rsa.pub"
+if [[ $(uname) == "Darwin" ]]; then
+    alias copyssh="pbcopy < $HOME/.ssh/id_rsa.pub"
+fi
 weather() { curl -4 wttr.in/${1:-seoul} }
 function gi() { curl -sLw n https://www.gitignore.io/api/$@ ;}
