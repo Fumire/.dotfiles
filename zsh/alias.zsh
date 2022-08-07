c2c61bdca3ef8b95ef39fa0022ad8ac73d821fcc
Jaewoong Lee <230@fumire.moe>
2022-08-07 22:57:16 +0900

    Update alias

--- zsh/alias.zsh
+++ zsh/alias.zsh
@@ -12,11 +12,15 @@ alias cp='cp -i'
 alias py='python3'
 
 # PIP update
-alias pipUpdate="pip3 freeze --local | grep -v '^\-e' | cut -d = -f 1 | xargs -n1 pip3 install -U"
+alias pipUpdate="pip3 install -U pip && pip3 freeze --local | grep -v '^\-e' | cut -d = -f 1 | xargs -n1 pip3 install -U"
 
 # Shortcuts
 if [[ $(uname) == "Darwin" ]]; then
     alias copyssh="pbcopy < $HOME/.ssh/id_rsa.pub"
 fi
-weather() { curl -4 wttr.in/${1:-seoul} }
-function gi() { curl -sLw n https://www.gitignore.io/api/$@ ;}
+
+function weather() { curl https://wttr.in/${1:-seoul}?m ;}
+
+function gi() { curl -sL https://www.gitignore.io/api/$@ ;}
+
+function cheat() { curl https://cheat.sh/$@ ;}
