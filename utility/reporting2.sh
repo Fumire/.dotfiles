6bed5dc48cd5a2155475e705640f2c8642261181
Jaewoong Lee <230@fumire.moe>
2023-04-16 19:41:14 +0900

    Update date

--- utility/reporting2.sh
+++ utility/reporting2.sh
@@ -4,4 +4,4 @@ IFS=$'\n\t'
 # sadf -g -O autoscale,showinfo,packed,height=700 -T -- -r -u /var/log/sysstat/sa$(date +%d) > $(hostname).svg
 sadf -g -O autoscale,showinfo,packed,height=700 -T -- -b -P all -r -u /var/log/sysstat/sa$(date +%d) > $(hostname).svg
 convert -density 300 -background none -crop x3300+0+0 $(hostname).svg $(hostname).png
-date | mutt -s "Daily Usage of $(hostname): $(date +%F)" -a $(hostname).png -- "root@compbio.unist.ac.kr"
+date -Iseconds | mutt -s "Daily Usage of $(hostname): $(date +%F)" -a $(hostname).png -- "root@compbio.unist.ac.kr"
