59fd082e661e96bd46674af65515d1672127451c
Jaewoong Lee <230@fumire.moe>
2024-05-14 15:17:27 +0900

    Minor changes
    Signed-off-by: Jaewoong Lee <230@fumire.moe>

--- utility/Backup.sh
+++ utility/Backup.sh
@@ -3,5 +3,5 @@
 # Last modified: 2023-11-21
 set -euo pipefail
 IFS=$'\n\t'
-mkdir /BiO/Backup/`date +%Y%m%d`
-tar -czpf /BiO/Backup/`date +%Y%m%d`/`date +%Y%m%d`.tgz /etc/passwd /etc/group /etc/shadow 1>/dev/null 2>&1
+mkdir /BiO/Backup/$(date +%Y%m%d)
+tar -czpf /BiO/Backup/$(date +%Y%m%d)/$(date +%Y%m%d).tgz /etc/passwd /etc/group /etc/shadow 1>/dev/null 2>&1
