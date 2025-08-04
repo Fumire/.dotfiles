c45ce2e58dac4aeda43717cc75cf280556bb2da1
Jaewoong Lee <230@fumire.moe>
2025-08-04 17:12:40 +0900

    Update migration script
    Signed-off-by: Jaewoong Lee <230@fumire.moe>

--- utility/migration_move.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-# Maintainer: jwlee230@unist.ac.kr
-set -euo pipefail
-IFS=$'\n\t'
-rsync -alrtvzLP -e 'ssh -p 3030 -c aes256-cbc' $(realpath .)
--- utility/migration_store.sh
+++ utility/migration_store.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
+#SBATCH --chdir=$(realpath .)
+#SBATCH --job-name=$(hostname)_$(basename $(realpath .))
+#SbATCH --mem=10G
+#SBATCH --cpus-per-task=1
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user='root@compbio.unist.ac.kr'
+#SBATCH --output=~/%x_%A.txt
+#SBATCH --error=~/%x_%A.txt
 # Maintainer: jwlee230@unist.ac.kr
-set -euo pipefail
-IFS=$'\n\t'
 tree -ls | tee tree.txt 
 find -L -type f ! -name 'tree.txt' -exec md5sum '{}' \; | tee md5.txt
+rsync -alrtvzLP -e 'ssh -p 3030 -c aes256-cbc' $(realpath .) root@kimura.kogic.kr:/BiO/Archive/
+rm -rfv $(realpath .)
