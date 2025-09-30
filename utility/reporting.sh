ff628f2674d9cce5223da9bcc6beb30211b5546f
Jaewoong Lee <230@fumire.moe>
2025-09-30 19:51:36 +0900

    Add list
    Signed-off-by: Jaewoong Lee <230@fumire.moe>

--- utility/reporting.sh
+++ utility/reporting.sh
@@ -4,16 +4,18 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-for h in 'host1' 'host2'; do
+hosts = ("host1", "host2")
+
+for h in ${hosts[@]}; do
     scp -c aes256-cbc -P $PORT jwlee230@$h.kogic.kr:/var/log/sysstat/sa$(date +%d) $h
 done
-for h in 'host1' 'host2'; do
+for h in ${hosts[@]}; do
     sadf -g -O autoscale,showinfo,packed,height=700 -T -- -r -u $h > $h.svg
     convert -density 500 -crop 5500x3500+0+0 $h.svg /var/www/html/image/top/$h.jpg &
 done
 wait
 
-for h in 'host1' 'host2'; do
+for h in ${hosts[@]}; do
     rm -fv $h*
 done
 
