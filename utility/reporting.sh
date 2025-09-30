8a4084c72dc2d640240fa91ad037d3c7ca63d95c
Jaewoong Lee <230@fumire.moe>
2025-09-30 19:52:02 +0900

    Minor changes
    Signed-off-by: Jaewoong Lee <230@fumire.moe>

--- utility/reporting.sh
+++ utility/reporting.sh
@@ -1,22 +1,21 @@
 #!/bin/bash
 # Maintainer: jwlee230@unist.ac.kr
-# Last modified: 2023-11-21
 set -euo pipefail
 IFS=$'\n\t'
 
 hosts = ("host1", "host2")
 
 for h in ${hosts[@]}; do
-    scp -c aes256-cbc -P $PORT jwlee230@$h.kogic.kr:/var/log/sysstat/sa$(date +%d) $h
+	scp -c aes256-cbc -P $PORT jwlee230@$h.kogic.kr:/var/log/sysstat/sa$(date +%d) $h
 done
 for h in ${hosts[@]}; do
-    sadf -g -O autoscale,showinfo,packed,height=700 -T -- -r -u $h > $h.svg
-    convert -density 500 -crop 5500x3500+0+0 $h.svg /var/www/html/image/top/$h.jpg &
+	sadf -g -O autoscale,showinfo,packed,height=700 -T -- -r -u $h > $h.svg
+	convert -density 500 -crop 5500x3500+0+0 $h.svg /var/www/html/image/top/$h.jpg &
 done
 wait
 
 for h in ${hosts[@]}; do
-    rm -fv $h*
+	rm -fv $h*
 done
 
 attachments=()
