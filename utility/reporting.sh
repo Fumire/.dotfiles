486d9785a5f3538faca914996a8d8d3010034721
Jaewoong Lee <230@fumire.moe>
2025-10-01 19:28:54 +0900

    Update mail reporting system
    Signed-off-by: Jaewoong Lee <230@fumire.moe>

--- utility/reporting.sh
+++ utility/reporting.sh
@@ -3,11 +3,12 @@
 set -euo pipefail
 IFS=$'\n\t'
 
-hosts = ("host1", "host2")
+hosts=("host1" "host2")
 
 for h in ${hosts[@]}; do
 	scp -c aes256-cbc -P $PORT jwlee230@$h.kogic.kr:/var/log/sysstat/sa$(date +%d) $h
 done
+
 for h in ${hosts[@]}; do
 	sadf -g -O autoscale,showinfo,packed,height=700 -T -- -r -u $h > $h.svg
 	convert -density 500 -crop 5500x3500+0+0 $h.svg /var/www/html/image/top/$h.jpg &
@@ -18,10 +19,6 @@ for h in ${hosts[@]}; do
 	rm -fv $h*
 done
 
-attachments=()
-for file in /var/www/html/image/top/*.jpg; do
-	if [ -f $file ]; then
-		attachments+=(--attach "$file")
-	fi
+for h in ${hosts[@]}; do
+	date | mail --attach /var/www/html/image/top/$h.jpg --subject "Server report ($h) on $(date +%Y%m%d)" -- "root@compbio.unist.ac.kr"
 done
-date | mail ${attachments[@]} --subject "Server report on $(date +%Y%m%d)" -- "root@compbio.unist.ac.kr"
