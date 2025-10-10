510c8a709060f6946dc1d0f21fa830e2278308c2
Jaewoong Lee <230@fumire.moe>
2025-10-10 14:15:44 +0900

    Update whisper script
    Signed-off-by: Jaewoong Lee <230@fumire.moe>

--- utility/whisper.sh
+++ utility/whisper.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+set -euo pipefail
+IFS=$'\n\t'
 
 for f in $@; do
 	if [[ -f ${f%.mp4}.srt ]]; then
@@ -6,7 +8,7 @@ for f in $@; do
 	fi
 	echo $f
 	ffmpeg -i $f ${f%.mp4}.mp3
-	whisper-cpp -m /Users/fumire/Library/CloudStorage/Dropbox/AI/whisper-model/ggml-large-v3-turbo.bin -osrt -l ko -t 8 -p 8 -f ${f%.mp4}.mp3
+	whisper-cli -m /Users/fumire/Library/CloudStorage/Dropbox/AI/whisper-model/ggml-large-v3-turbo.bin -osrt -l ko -t 8 -p 8 -f ${f%.mp4}.mp3
 	mv -v ${f%.mp4}.mp3.srt ${f%.mp4}.srt
 	rm -fv ${f%.mp4}.mp3
 done
