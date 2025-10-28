68b3c91dcd7ab96a359fbc250fe22ea019d719c1
Jaewoong Lee <230@fumire.moe>
2025-10-28 19:08:28 +0900

    Update nvim plugin
    Signed-off-by: Jaewoong Lee <230@fumire.moe>

--- nvim/plugin.lua
+++ nvim/plugin.lua
@@ -197,7 +197,7 @@ return {
         "neoclide/coc.nvim",
         branch = "release",
         config = function()
-            vim.g.coc_global_extensions = { "coc-json", "coc-tsserver", "coc-pyright", "coc-tabnine" }
+            vim.g.coc_global_extensions = { "coc-json", "coc-tsserver", "coc-tabnine", "coc-jedi" }
         end,
     },
 
