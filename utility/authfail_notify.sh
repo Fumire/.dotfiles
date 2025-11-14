75d91e2eb78313bfc40948ab37068d6e0a76cc15
Jaewoong Lee <230@fumire.moe>
2025-11-14 18:53:43 +0900

    Add authfail notification
    Signed-off-by: Jaewoong Lee <230@fumire.moe>

---
+++ utility/authfail_notify.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+# Maintainer: jaewoong@unist.ac.kr
+set -euo pipefail
+IFS=$'\n\t'
+
+if [ -z "$PAM_USER" ]; then
+    exit 0
+fi
+
+/usr/bin/mail -s "SSH-$HOSTNAME" "root@compbio.unist.ac.kr" <<< "Failed login attempt in ${PAM_USER} on $(date)"
+
+exit 0
