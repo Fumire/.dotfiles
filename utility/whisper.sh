1abe027941f39829f95759a9bdf4eb33e1315524
Jaewoong Lee <230@fumire.moe>
2025-11-28 14:59:14 +0900

    Update file path
    Signed-off-by: Jaewoong Lee <230@fumire.moe>

--- utility/whisper.sh
+++ utility/whisper.sh
@@ -8,7 +8,7 @@ for f in "$@"; do
 	fi
 	echo "$f"
 	ffmpeg -i "$f" "${f%.mp4}.mp3"
-	whisper-cli -m /Users/fumire/Library/CloudStorage/Dropbox/AI/whisper-model/ggml-large-v3-turbo.bin -osrt -l ko -t 8 -p 8 -f "${f%.mp4}.mp3"
+	whisper-cli -m "/Users/fumire/Library/CloudStorage/Dropbox/31_AI/whisper-model/ggml-large-v3-turbo.bin" -osrt -l ko -t 8 -p 8 -f "${f%.mp4}.mp3"
 	mv -v "${f%.mp4}.mp3.srt" "${f%.mp4}.srt"
 	rm -fv "${f%.mp4}.mp3"
 done
