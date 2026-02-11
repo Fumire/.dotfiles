3dbb51dac3b1e698ff7de69dcac6065bfaa352a0
Jaewoong Lee <230@fumire.moe>
2026-02-11 19:16:26 +0900

    Update Whisper command
    Signed-off-by: Jaewoong Lee <230@fumire.moe>

--- utility/whisper.sh
+++ utility/whisper.sh
@@ -10,7 +10,7 @@ for f in "$@"; do
 	fi
 	echo "$f"
     ffmpeg -y -i "$f" -ar 16000 -ac 1 -c:a pcm_s16le "${f%.mp4}.wav"
-    whisper-cli -m "/Users/fumire/Library/CloudStorage/Dropbox/31_AI/whisper-model/ggml-large-v3.bin" --output-srt --language ko --threads 8 --processors 8 --file "${f%.mp4}.wav"
+    whisper-cli -m "/Users/fumire/Library/CloudStorage/Dropbox/31_AI/whisper-model/ggml-large-v3.bin" --output-srt --language ko --threads 8 --processors 8 --print-colors --print-confidence --file "${f%.mp4}.wav"
 	mv -v "${f%.mp4}.wav.srt" "${f%.mp4}.srt"
 	rm -fv "${f%.mp4}.wav"
 done
