cc82a321df1239d5379a8f020f93fe57c77ff91b
Jaewoong Lee <230@fumire.moe>
2026-04-19 16:46:55 -0700

    Add ACC and LANG option
    Signed-off-by: Jaewoong Lee <230@fumire.moe>

--- utility/whisper.sh
+++ utility/whisper.sh
@@ -13,7 +13,7 @@ for f in "$@"; do
             continue
         fi
         ffmpeg -y -i "$f" -q:a 0 -map a "${f/%.mp4/.mp3}"
-        whisper-cli -m "/Users/fumire/Library/CloudStorage/Dropbox/31_AI/whisper-model/ggml-large-v3.bin" --output-srt --language ko --threads 8 --processors 8 --print-colors --print-confidence --file "${f/%.mp4/.mp3}"
+        whisper-cli -m "/Users/fumire/Library/CloudStorage/Dropbox/31_AI/whisper-model/ggml-large-v3.bin" --output-srt --language "${lang:-ko}" --threads 8 --processors 8 --print-colors --print-confidence --file "${f/%.mp4/.mp3}"
         mv -v "${f/%.mp4/.mp3.srt}" "${f/%.mp4/.srt}"
         rm -fv "${f/%.mp4/.mp3}"
     elif [[ "$f" == *.m4a ]]; then
@@ -21,14 +21,22 @@ for f in "$@"; do
             continue
         fi
         ffmpeg -i "$f" -c:v copy -c:a libmp3lame -q:a 4 "${f/%.m4a/.mp3}"
-        whisper-cli -m "/Users/fumire/Library/CloudStorage/Dropbox/31_AI/whisper-model/ggml-large-v3.bin" --output-srt --language ko --threads 8 --processors 8 --print-colors --print-confidence --file "${f/%.m4a/.mp3}"
+        whisper-cli -m "/Users/fumire/Library/CloudStorage/Dropbox/31_AI/whisper-model/ggml-large-v3.bin" --output-srt --language "${lang:-ko}" --threads 8 --processors 8 --print-colors --print-confidence --file "${f/%.m4a/.mp3}"
         mv -v "${f/%.m4a/.mp3.srt}" "${f/%.m4a/.srt}"
         rm -fv "${f/%.m4a/.mp3}"
+    elif [[ "$f" == *.aac ]]; then
+        if [[ -f "${f/%.aac/.srt}" ]]; then
+            continue
+        fi
+        ffmpeg -i "$f" -acodec libmp3lame "{$f/%.aac/.mp3}"
+        whisper-cli -m "/Users/fumire/Library/CloudStorage/Dropbox/31_AI/whisper-model/ggml-large-v3.bin" --output-srt --language "${lang:-ko}" --threads 8 --processors 8 --print-colors --print-confidence --file "${f/%.aac/.mp3}"
+        mv -v "${f/%.aac/.mp3.srt}" "${f/%.aac/.srt}"
+        rm -fv "${f/%.aac/.mp3}"
     elif [[ "$f" == *.mp3 ]]; then
         if [[ -f "${f/%.mp3/.srt}" ]]; then
             continue
         fi
-        whisper-cli -m "/Users/fumire/Library/CloudStorage/Dropbox/31_AI/whisper-model/ggml-large-v3.bin" --output-srt --language ko --threads 8 --processors 8 --print-colors --print-confidence --file "$f"
+        whisper-cli -m "/Users/fumire/Library/CloudStorage/Dropbox/31_AI/whisper-model/ggml-large-v3.bin" --output-srt --language "${lang:-ko}" --threads 8 --processors 8 --print-colors --print-confidence --file "$f"
         mv -v "${f/%.mp3/.mp3.srt}" "${f/%.mp3/.srt}"
     fi
 done
