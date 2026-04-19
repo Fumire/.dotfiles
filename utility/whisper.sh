79ae7a5d7df72c9595948111400ade72da6b1be9
Jaewoong Lee <230@fumire.moe>
2026-04-20 08:31:58 +0900

    Update whisper
    Signed-off-by: Jaewoong Lee <230@fumire.moe>

--- utility/whisper.sh
+++ utility/whisper.sh
@@ -1,17 +1,34 @@
 #!/bin/bash
-# Maintainer: jaewoong@unist.ac.kr
+# Maintainer: 230@fumire.moe
 set -euo pipefail
 IFS=$'\n\t'
 
 export PATH="/opt/homebrew/bin:/opt/homebrew/sbin:$PATH"
 
 for f in "$@"; do
-	if [[ -f ${f%.mp4}.srt ]]; then
-		continue
-	fi
 	echo "$f"
-    ffmpeg -y -i "$f" -ar 16000 -ac 1 -c:a pcm_s16le "${f%.mp4}.wav"
-    whisper-cli -m "/Users/fumire/Library/CloudStorage/Dropbox/31_AI/whisper-model/ggml-large-v3.bin" --output-srt --language ko --threads 8 --processors 8 --print-colors --print-confidence --file "${f%.mp4}.wav"
-	mv -v "${f%.mp4}.wav.srt" "${f%.mp4}.srt"
-	rm -fv "${f%.mp4}.wav"
+
+    if [[ "$f" == *.mp4 ]]; then
+        if [[ -f "${f/%.mp4/.srt}" ]]; then
+            continue
+        fi
+        ffmpeg -y -i "$f" -q:a 0 -map a "${f/%.mp4/.mp3}"
+        whisper-cli -m "/Users/fumire/Library/CloudStorage/Dropbox/31_AI/whisper-model/ggml-large-v3.bin" --output-srt --language ko --threads 8 --processors 8 --print-colors --print-confidence --file "${f/%.mp4/.mp3}"
+        mv -v "${f/%.mp4/.mp3.srt}" "${f/%.mp4/.srt}"
+        rm -fv "${f/%.mp4/.mp3}"
+    elif [[ "$f" == *.m4a ]]; then
+        if [[ -f "${f/%.m4a/.srt}" ]]; then
+            continue
+        fi
+        ffmpeg -i "$f" -c:v copy -c:a libmp3lame -q:a 4 "${f/%.m4a/.mp3}"
+        whisper-cli -m "/Users/fumire/Library/CloudStorage/Dropbox/31_AI/whisper-model/ggml-large-v3.bin" --output-srt --language ko --threads 8 --processors 8 --print-colors --print-confidence --file "${f/%.m4a/.mp3}"
+        mv -v "${f/%.m4a/.mp3.srt}" "${f/%.m4a/.srt}"
+        rm -fv "${f/%.m4a/.mp3}"
+    elif [[ "$f" == *.mp3 ]]; then
+        if [[ -f "${f/%.mp3/.srt}" ]]; then
+            continue
+        fi
+        whisper-cli -m "/Users/fumire/Library/CloudStorage/Dropbox/31_AI/whisper-model/ggml-large-v3.bin" --output-srt --language ko --threads 8 --processors 8 --print-colors --print-confidence --file "$f"
+        mv -v "${f/%.mp3/.mp3.srt}" "${f/%.mp3/.srt}"
+    fi
 done
