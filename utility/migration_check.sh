90e8d6cca1893f5c1bc14f55f6df9d5e8af02d4c
Jaewoong Lee <230@fumire.moe>
2026-06-04 17:41:36 +0900

    Make migration check Slurm-friendly

--- utility/migration_check.sh
+++ utility/migration_check.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-#SBATCH --chdir=$(realpath .)
-#SBATCH --job-name=$(hostname)_$(basename $(realpath .))
+#SBATCH --chdir=.
+#SBATCH --job-name=Migration_check
 #SBATCH --mem=10G
 #SBATCH --cpus-per-task=1
 #SBATCH --mail-type=ALL
@@ -11,4 +11,4 @@
 set -euo pipefail
 IFS=$'\n\t'
 tree -ls -I "tree.txt|md5.txt" | diff - tree.txt
-md5sum --quiet -c md5.txt
+md5sum -c md5.txt 2>&1 | mail -s "MD5 check report for $(hostname): $(basename "$(realpath .)")" "root@compbio.unist.ac.kr"
