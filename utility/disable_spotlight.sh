42465b265dca1a5de78cd130e8e5030a32573af9
Jaewoong Lee <230@fumire.moe>
2026-06-04 17:39:00 +0900

    Guard disable_spotlight.sh for macOS

--- utility/disable_spotlight.sh
+++ utility/disable_spotlight.sh
@@ -3,6 +3,11 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+if [[ $(uname -s) != "Darwin" ]]; then
+    echo "disable_spotlight.sh is only supported on macOS." >&2
+    exit 0
+fi
+
 for f in "$@"; do
     touch "$f/.metadata_never_index"
     dot_clean -mv "$f"
